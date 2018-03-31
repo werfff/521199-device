7d1813d0cbc2d5a58ea62dc324f8c882732cad9c
Your Name <your_email@whatever.com>
2018-03-30 22:48:03 -0700

    03/30/2018
    V1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Папка для скриптов.
+//
+// --
+//
+// Пример структуры файлов:
+//
+// js/
+// | - script.js
+// | - [другие *.js файлы]
+//
+
+// function demoDisplay() {
+//  document.getElementById("myP1").style.display = "none";
+// }
+
+$('.map').on('click', function(){
+     $('.map-popup').css('display','block');
+});
